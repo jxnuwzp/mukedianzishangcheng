9db823d6269464a59648ad68b8b6a2649a5735ae
Singlepig001 <826739558@qq.com>
2017-02-19 20:21:33 +0800

    thumb function
    加入缩略图功能

--- shopimooc/lib/image.func.php
+++ shopimooc/lib/image.func.php
@@ -41,3 +41,33 @@ function verifyImage($type=1,$length=4,$pixel=0,$line=0,$sess_name = "verify"){
 	imagegif ( $image );
 	imagedestroy ( $image );
 }
+
+//生成缩略图
+function thumb($filename,$destination=null,$dst_w=null,$dst_h=null,$isReservedSource=false,$scale=0.5){
+    list($src_w,$src_h,$imagetype)=getimagesize($filename);
+    //$scale=0.5;
+    if (is_null($dst_w)||is_null($dst_h)){
+        $dst_w=ceil($src_w*$scale);
+        $dst_h=ceil($src_h*$scale);
+    }
+    $mime=image_type_to_mime_type($imagetype);
+    $createFun=str_replace("/","createfrom",$mime);
+    $outFun=str_replace("/",null,$mime);
+    $src_image=$createFun($filename);
+    $dst_image=imagecreatetruecolor($dst_w,$dst_h);
+    imagecopyresampled($dst_image,$src_image,0,0,0,0,$dst_w,$dst_h,$src_w,$src_h);
+    //image_50
+    if ($destination&&!file_exists(dirname($destination))){
+        mkdir(dirname($destination),0777,true);
+    }
+    $dstFilename=$destination==null?getUniName().".".getExt($filename):$destination;
+    $outFun($dst_image,$dstFilename);
+    imagedestroy($src_image);
+    imagedestroy($dst_image);
+    //$isReservedSource=false;
+    if (!$isReservedSource){
+        unlink($filename);
+    }
+    return $dstFilename;
+
+}
